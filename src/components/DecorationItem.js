48703bbad13b4c630fc82e0eebdaeb913691fa40
Dmitrii_Panov <panov.alchemic@gmail.com>
2020-06-30 14:53:07 +0300

    add bootstrap

--- src/components/DecorationItem.js
+++ src/components/DecorationItem.js
@@ -1,22 +1,34 @@
-import React, { useContext } from 'react';
-import {Group, Card} from '@vkontakte/vkui'
-import { AppContext } from '../App';
+import React, { useContext } from "react";
+import { AppContext } from "../App";
+import { Card } from "react-bootstrap";
 
 export const DecorationItem = () => {
-
   const context = useContext(AppContext);
-  const handleClick = () => { 
-    context.setActivePanel('decorationView')
-  }
+  const handleClick = () => {
+    context.setActivePanel("decorationView");
+  };
 
   return (
-    <Group>
-      <Card size="l" mode="outline" onClick={handleClick}>
-        <div style={{ height: 200, overflow: 'hidden', position: 'relative', borderRadius: 8 }} >
-          <img width='100%' src='https://sun9-26.userapi.com/c857416/v857416725/d65b4/T8PeuTLKcAc.jpg'/>
-        </div>
-      </Card>
-  </Group>
+    <Card
+      bg="info"
+      key={1}
+      text="white"
+      style={{ width: "100%" }}
+      className="mb-2"
+      onClick={handleClick}
+    >
+      {/* <Card.Header>Header</Card.Header> */}
+      <Card.Img
+        variant="top"
+        src="https://sun9-26.userapi.com/c857416/v857416725/d65b4/T8PeuTLKcAc.jpg"
+      />
+      <Card.Body>
+        <Card.Title>House</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
-
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,28 +1,36 @@
-import React, { useContext } from 'react';
-import {Group, CardGrid, Card} from '@vkontakte/vkui';
-import Icon28FavoriteOutline from '@vkontakte/icons/dist/28/favorite_outline';
-import { AppContext } from '../App';
+import React, { useContext } from "react";
+import { AppContext } from "../App";
+import { Card } from "react-bootstrap";
 
 const Item = (props) => {
-  const {onClick} = props;
-  const context = useContext(AppContext)
+  const { onClick } = props;
+  const context = useContext(AppContext);
 
+  const handleClick = () => {
+    context.setActivePanel("productView");
+  };
 
-  const handleClick = () => { 
-    context.setActivePanel('productView')
-  }
-  
   return (
-    <Group>
-      <Card size="l" mode="outline" onClick={handleClick}>
-        <div style={{ height: 200, overflow: 'hidden', position: 'relative', borderRadius: 8 }} >
-          <img width='100%' src='https://sun9-41.userapi.com/impf/c850624/v850624204/3bd13/egumRJFFvmk.jpg?size=520x0&quality=90&sign=6247c7fa72c2b11f89865889e655d87b'/>
-          <div style={{color: '#fff', position: 'absolute', top: 5, right: 5}}>
-          <Icon28FavoriteOutline/>
-          </div>
-        </div>
-      </Card>
-  </Group>
+    <Card
+      bg="primary"
+      key={1}
+      text="white"
+      style={{ width: "100%" }}
+      className="mb-2"
+      onClick={handleClick}
+    >
+      <Card.Img
+        variant="top"
+        src="https://sun9-69.userapi.com/impf/c855432/v855432199/149ee3/0hB4wtXBytc.jpg?size=520x0&quality=90&sign=c0af08c888b570f1dc6145369e10603e"
+      />
+      <Card.Body>
+        <Card.Title>Light</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
